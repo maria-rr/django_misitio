1eda9d023b85e910666e9f9e96052fea64d799da
maria <mounstrigirl@gmail.com>
2025-09-20 10:23:12 -0600

    Se agrega configuracion de CSRF_TRUSTED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["localhost"])
+
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 
 # Application definition
